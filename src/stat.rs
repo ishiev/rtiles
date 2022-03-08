d09d09163f27ce75d877f27c08dc2cf4d124efe1
Nikolay Ishiev <nikolay.ishiev@gmail.com>
2022-03-08 13:56:16 +0300

    Add stat server

--- src/stat.rs
+++ src/stat.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::ops::AddAssign;
 use std::sync::Arc;
+use tokio::task::{self, JoinHandle};
 use tokio::sync::{mpsc, RwLock};
 
 /// Statistic key
@@ -117,7 +118,40 @@ impl StatTable {
 #[derive(Clone)]
 struct Stat {
     all: Arc<StatTable>,
-    sender: mpsc::Sender<Record>
+    tx: mpsc::Sender<Record>,
+}
+
+impl Stat {
+    fn new() -> Self {
+        let all = Arc::new(StatTable::new());
+        let all_rx = Arc::clone(&all);
+        let (tx, mut rx) = mpsc::channel(500);
+        
+        // spawn a detached async task
+        // task ended when the channel has been closed 
+        task::spawn(async move {
+            debug!("stat recv task started");
+            while let Some(rec) = rx.recv().await {
+                // insert record to stat table
+                all_rx.insert(rec).await;
+            }
+            debug!("stat recv task finished");
+        });
+
+        Stat { all, tx }
+    }
+
+    async fn insert(&self, rec: Record) 
+        -> Result<(), mpsc::error::SendError<Record>> {
+        self.tx.send(rec).await
+    }
+
+    async fn get(&self, key: &StatKey) -> Metrics {
+        // move current task to end of the task queue 
+        // to complete async inserts before get back values
+        task::yield_now().await;
+        self.all.get(key).await
+    }
 }
 
 
@@ -179,5 +213,27 @@ mod test {
         key.model = None;
         res = stat.get(&key).await;
         assert_eq!(res, Metrics { hits: 5, bytes: 5000 });
+    }
+
+    #[tokio::test]
+    async fn stat_server() {
+        let mut key = StatKey {
+            object: Some("city".to_owned()),
+            model: Some("block".to_owned())
+        };
+        let metrics = Metrics { hits: 1, bytes: 1000 };
+        let stat = Stat::new();
+
+        for _ in 0..10 {
+            stat.insert(Record { key: key.clone(), metrics }).await.unwrap();
+        }
+        let mut res = stat.get(&key).await;
+        assert_eq!(res, Metrics { hits: 10, bytes: 10000 });
+
+        // test metrics for server
+        key.object = None;
+        key.model = None;
+        res = stat.get(&key).await;
+        assert_eq!(res, Metrics { hits: 10, bytes: 10000 });
     }
 }
