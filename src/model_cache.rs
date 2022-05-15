8b6ff2a16a71b9c93d0f773c9ffec3a7b28bcbd5
Nikolay Ishiev <nikolay.ishiev@gmail.com>
2022-05-15 23:22:47 +0300

    Add CachedNamedFile type

--- src/model_cache.rs
+++ src/model_cache.rs
@@ -1,15 +1,22 @@
 use bytes::Bytes;
 use moka::future::Cache;
+
+use rocket::fs::NamedFile;
 use rocket::http::ContentType;
+use rocket::request::Request;
+use rocket::response::{self, Responder, Response};
 use rocket::serde::{Deserialize, Serialize};
+
+use std::io::Cursor;
 use std::path::{Path, PathBuf};
 use std::time::Duration;
+
 use tokio::fs::File;
 use tokio::io::{self, AsyncReadExt};
 use tokio::sync::mpsc;
 use tokio::task;
 
-/// Model file cache configuration
+/// File cache configuration
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
 pub struct FileCacheConfig {
     pub size: u64,      // cache size limit in bytes
@@ -27,10 +34,65 @@ impl Default for FileCacheConfig {
     }
 }
 
+
+pub enum CachedNamedFile {
+    File(NamedFile),
+    Cache(Content)
+}
+
+impl CachedNamedFile {
+    /// Get back cached content or open named file
+    pub async fn open_with_cache(path: PathBuf, cache: &FileCache) 
+    -> io::Result<CachedNamedFile> {
+        if let Some(cnt) = cache.get(&path) {
+            Ok(CachedNamedFile::Cache(cnt))
+        } else {
+            let f = NamedFile::open(&path).await;
+            if let Ok(_) = f {
+                // insert new file to cache only if open is Ok
+                cache.insert(path).unwrap_or_else(
+                    | err | error!("error insert new path to cache: {}", err)
+                )
+            }
+            f.map(|x| CachedNamedFile::File(x))
+        }
+    }
+
+    /// Get content lenght
+    pub async fn len(&self) -> usize {
+        match self {
+            CachedNamedFile::File(f) => 
+                match f.metadata().await {
+                    Ok(m) => m.len() as usize,
+                    Err(_) => 0
+                }
+            CachedNamedFile::Cache(c) => c.length
+        }
+    }
+
+    pub fn is_cached(&self) -> bool {
+        match self {
+            CachedNamedFile::File(_) => false,
+            CachedNamedFile::Cache(_) => true
+        }
+    }
+}
+
+/// Combined responder for named file and cached content
+impl<'r> Responder<'r, 'static> for CachedNamedFile {
+    fn respond_to(self, req: &'r Request<'_>) -> response::Result<'static> {
+        match self {
+            CachedNamedFile::File(f) => f.respond_to(req),
+            CachedNamedFile::Cache(c) => c.respond_to(req),
+        }
+    }
+}
+
+
 /// Saved content
 #[derive(Clone)]
 pub struct Content {
-    length: u64,                    // size in bytes
+    length: usize,                  // size in bytes
     mime_type: Option<ContentType>, // content mime type
     body: Bytes,                    // body in-memory buffer
 }
@@ -41,20 +103,21 @@ impl Content {
         // open file for reading
         let mut f = File::open(&path).await?;
 
-        // get file length
-        let length = f.metadata().await?.len();
+        // get content length
+        let length = f.metadata().await?.len() as usize;
 
-        // parse content type from file extension
+        // parse content type from file extension if the extension is
+        // recognized. See [`ContentType::from_extension()`] for more information.
         let mime_type = match path.as_ref().extension() {
             Some(ext) => ContentType::from_extension(&ext.to_string_lossy()),
             None => None,
         };
         
         // read the whole file to
-        let mut buf = Vec::with_capacity(length as usize);
+        let mut buf = Vec::with_capacity(length);
         let bytes = f.read_to_end(&mut buf).await?;
 
-        assert_eq!(bytes as u64, length);
+        assert_eq!(bytes, length);
 
         Ok(Content {
             length,
@@ -64,6 +127,15 @@ impl Content {
     }
 }
 
+/// Streams the content to the client
+impl<'r> Responder<'r, 'static> for Content {
+    fn respond_to(self, _: &'r Request<'_>) -> response::Result<'static> {
+        Response::build()
+            .header(self.mime_type.unwrap_or(ContentType::Binary))
+            .sized_body(Some(self.length), Cursor::new(self.body))
+            .ok()
+    }
+}
 
 /// File cache
 pub struct FileCache {
